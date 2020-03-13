dfdb9c99e65230a8a994fbbe92d26255b541e97f
Wilson Ng <51449968+wilsonng21@users.noreply.github.com>
2020-03-13 11:25:31 -0400

    Create loops_practice.py

---
+++ loops_practice.py
@@ -0,0 +1,12 @@
+single_digits = []
+single_digits = list(range(10))
+
+squares = []
+
+for digit in single_digits:
+  print(digit)
+  squares.append(digit ** 2)
+  print(squares)
+  
+cubes = [digit ** 3 for digit in single_digits]
+print(cubes)
