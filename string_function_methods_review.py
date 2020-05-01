b113db499227f668b52c8913d8f668c9935e08a3
Wilson Ng <51449968+wilsonng21@users.noreply.github.com>
2020-05-01 19:43:09 -0400

    Create string_function_methods_review.py

---
+++ string_function_methods_review.py
@@ -0,0 +1,28 @@
+highlighted_poems = "Afterimages:Audre Lorde:1997,  The Shadow:William Carlos Williams:1915, Ecstasy:Gabriela Mistral:1925,   Georgia Dusk:Jean Toomer:1923,   Parting Before Daybreak:An Qi:2014, The Untold Want:Walt Whitman:1871, Mr. Grumpledump's Song:Shel Silverstein:2004, Angel Sound Mexico City:Carmen Boullosa:2013, In Love:Kamala Suraiyya:1965, Dream Variations:Langston Hughes:1994, Dreamwood:Adrienne Rich:1987"
+
+
+highlighted_poems_list = highlighted_poems.split(',')
+
+highlighted_poems_stripped = []
+
+for lst in highlighted_poems_list:
+  highlighted_poems_stripped.append(lst.strip())
+
+highlighted_poems_details = []
+
+for item in highlighted_poems_stripped:
+  highlighted_poems_details.append(item.split(':'))
+
+print(highlighted_poems_details)
+titles = []
+poets = []
+dates = []
+
+for item in highlighted_poems_details:
+  titles.append(item[0])
+  poets.append(item[1])
+  dates.append(item[2])
+
+for i in range(len(titles)):
+  print("The poem {titles} was published by {poets} in {dates}.".format(titles=titles[i], poets=poets[i], dates=dates[i]))
+
