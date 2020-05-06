2efd3b49e66e59af2a413f3dfeb293279da34284
Wilson Ng <51449968+wilsonng21@users.noreply.github.com>
2020-05-06 19:10:51 -0400

    Update str_coding_challenges.py

--- str_coding_challenges.py
+++ str_coding_challenges.py
@@ -36,3 +36,17 @@ print(substring_between_letters("apple", "p", "e"))
 print(substring_between_letters("apple", "p", "c"))
 # should print "apple"
 
+# Write your x_length_words function here:
+def x_length_words(sentence, x):
+  lst = sentence.split()
+  for word in lst:
+    if len(word) >= x:
+      return True
+    else:
+      return False
+# Uncomment these function calls to test your tip function:
+print(x_length_words("i like apples", 2))
+# should print False
+print(x_length_words("he likes apples", 2))
+# should print True
+
