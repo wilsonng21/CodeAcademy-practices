884969c7b4f85f8bc703a37b321507eceef7ba9f
Wilson Ng <51449968+wilsonng21@users.noreply.github.com>
2020-05-06 14:36:32 -0400

    Update str_coding_challenges.py

--- str_coding_challenges.py
+++ str_coding_challenges.py
@@ -21,3 +21,18 @@ print(count_multi_char_x("apple", "pp"))
 # should print 1
 
 #it should return the number of times x appears in word
+
+
+def substring_between_letters(word, start, end):
+  start_ind = word.find(start)
+  end_ind = word.find(end)
+  if start_ind > -1 and end_ind > -1:
+  	return(word[start_ind+1:end_ind])
+  return word
+
+# Uncomment these function calls to test your tip function:
+print(substring_between_letters("apple", "p", "e"))
+# should print "pl"
+print(substring_between_letters("apple", "p", "c"))
+# should print "apple"
+
