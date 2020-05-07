02ff4a5d77bec482d139590fa6b55d77f38cb3be
Wilson Ng <51449968+wilsonng21@users.noreply.github.com>
2020-05-07 18:16:44 -0400

    Update str_coding_challenges.py

--- str_coding_challenges.py
+++ str_coding_challenges.py
@@ -121,3 +121,13 @@ print(make_spoonerism("Hello", "world!"))
 print(make_spoonerism("a", "b"))
 # should print b a
 
+# Write your add_exclamation function here:
+def add_exclamation(word):
+  while len(word) < 20:
+    word += "!"
+  return word
+# Uncomment these function calls to test your function:
+print(add_exclamation("Codecademy"))
+# should print Codecademy!!!!!!!!!!
+print(add_exclamation("Codecademy is the best place to learn"))
+# should print Codecademy is the best place to learn
