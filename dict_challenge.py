9108b2b43dc7e8bfb19861ea609e54c7559c2c43
Wilson Ng <51449968+wilsonng21@users.noreply.github.com>
2020-05-24 12:27:56 -0400

    Update dict_challenge.py

--- dict_challenge.py
+++ dict_challenge.py
@@ -35,3 +35,90 @@ print(add_ten({1:5, 2:2, 3:3}))
 # should print {1:15, 2:12, 3:13}
 print(add_ten({10:1, 100:2, 1000:3}))
 # should print {10:11, 100:12, 1000:13
+
+# Write your values_that_are_keys function here:
+def values_that_are_keys(my_dictionary):
+  lst = []
+  for value in my_dictionary.values():
+    if value in my_dictionary.keys():
+      lst.append(value)
+  return lst
+# Uncomment these function calls to test your  function:
+print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
+# should print [1, 4]
+print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
+# should print ["a"]
+
+# Write your max_key function here:
+def max_key(my_dictionary):
+  largest_key = ''
+  largest_value = float("-inf")
+  for key, value in my_dictionary.items():
+    if value > largest_value:
+      largest_value = value
+      largest_key = key
+  return largest_key
+
+# Uncomment these function calls to test your  function:
+print(max_key({1:100, 2:1, 3:4, 4:10}))
+# should print 1
+print(max_key({"a":100, "b":10, "c":1000}))
+# should print "c"
+
+# Write your word_length_dictionary function here:
+def word_length_dictionary(words):
+  my_dict = {}
+  for word in words:
+    my_dict[word] = len(word)
+  return my_dict
+# Uncomment these function calls to test your  function:
+print(word_length_dictionary(["apple", "dog", "cat"]))
+# should print {"apple":5, "dog": 3, "cat":3}
+print(word_length_dictionary(["a", ""]))
+# should print {"a": 1, "": 0}
+
+
+# Write your frequency_dictionary function here:
+def frequency_dictionary(words):
+  my_dict = {}
+  for word in words:
+    if word not in my_dict:
+      my_dict[word] = 0
+    my_dict[word] += 1
+  return my_dict
+# Uncomment these function calls to test your  function:
+print(frequency_dictionary(["apple", "apple", "cat", 1]))
+# should print {"apple":2, "cat":1, 1:1}
+print(frequency_dictionary([0,0,0,0,0]))
+# should print {0:5}
+
+# Write your unique_values function here:
+def unique_values(my_dictionary):
+  seen_values = []
+  for value in my_dictionary.values():
+    if value not in seen_values:
+      seen_values.append(value)
+  return len(seen_values)
+
+# Uncomment these function calls to test your  function:
+print(unique_values({0:3, 1:1, 4:1, 5:3}))
+# should print 2
+print(unique_values({0:3, 1:3, 4:3, 5:3}))
+# should print 1
+
+# Write your count_first_letter function here:
+def count_first_letter(names):
+  new_dict = {}
+  for key in names:
+    first_letter = key[0]
+    if first_letter not in new_dict:
+      new_dict[first_letter] = 0
+    new_dict[first_letter] += len(names[key])
+  return new_dict
+
+# Uncomment these function calls to test your  function:
+print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}))
+# should print {"S": 4, "L": 3}
+print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Sannister": ["Jaime", "Cersei", "Tywin"]}))
+# should print {"S": 7}
+
